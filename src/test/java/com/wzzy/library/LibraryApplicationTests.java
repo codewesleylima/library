a415772c379353e43a65b87e4f15310fabaabba5
codewesleylima <gusterlima@hotmail.com>
2026-02-03 00:57:38 -0300

    Add basic test class

---
+++ src/test/java/com/wzzy/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wzzy.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
